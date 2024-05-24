83b1b1b8fa37d74ae33df031cf438f1078d6d80a
victor@victor <victor.dmaina@gmail.com>
2024-05-24 15:54:10 +0300

    detect anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(self.word)
+    
+    
+    def is_anagram(self, other_word):
+        return sorted(other_word.lower()) == self.sorted_word
+        
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
